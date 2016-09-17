7415a73944984e4e89648acff0cdc84bb2134a8d
Nicholas Angell <nrangell94@gmail.com>
2016-09-17 16:46:11 -0600

    Map and Hex edits

--- java/src/model/Hex.java
+++ java/src/model/Hex.java
@@ -1,7 +1,33 @@
 package model;
 
+import shared.locations.HexLocation;
+import model.ResourceCard.Resource;
+
 /**
  * Created by kcwillmore on 9/17/16.
  */
 public class Hex {
+	private Resource resource;
+	private HexLocation location;
+	private int numberToken;
+	
+	public Hex(Resource resource, HexLocation location, int numberToken) {
+		
+	}
+	
+	
+	
+	public Resource getResourceType() {
+		return resource;
+	}
+	
+	public HexLocation getLocation() {
+		return location;
+	}
+	
+	public int getNumberToken() {
+		return numberToken;
+	}
+	
+	
 }
--- java/src/model/Map.java
+++ java/src/model/Map.java
@@ -1,7 +1,27 @@
 package model;
 
+import java.util.List;
+
 /**
  * Created by kcwillmore on 9/17/16.
  */
 public class Map {
+	private List<Hex> hexes;
+	private List<Settlement> settlements;
+	private List<City> cities;
+	private List<Road> roads;
+	private List<Port> ports;
+	private Robber robber;
+	
+	public Map() {
+		int[] numberTokens = {2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12};
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 }
