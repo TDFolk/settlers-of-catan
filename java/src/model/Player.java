7584cd0f1d9fedb0044a0556dd164306e395c51a
KC Willmore <kcwillmore@KCs-MacBook-Pro.local>
2016-09-17 14:59:18 -0600

    :V

--- java/src/model/Player.java
+++ java/src/model/Player.java
@@ -1,7 +1,16 @@
 package model;
 
+import shared.definitions.CatanColor;
+
+import java.util.List;
+
 /**
  * Created by kcwillmore on 9/17/16.
  */
 public class Player {
+
+    private CatanColor color;
+    private List<Settlement> settlements;
+    private List<Road> roads;
+    
 }
