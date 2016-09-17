57619dfe315a7625ee5fd47c212fc7acaa85edd1
Brandon <bvance800@gmail.com>
2016-09-17 14:34:13 -0600

    Added constructor and private data members
    see above

--- java/src/server/ServerPoller.java
+++ java/src/server/ServerPoller.java
@@ -4,5 +4,14 @@ package server;
  * Created by jihoon on 9/16/2016.
  */
 public class ServerPoller {
-    
+
+    private IServer proxy;
+    private final long TIME_INTERVAL = 1000;
+
+    public ServerPoller(IServer proxyServer){
+
+        this.proxy = proxyServer;
+
+    }
+
 }
