180979748b130a3b5cb029571403aa81c853521a
Ji Hoon Park <jihoonpark4829@gmail.com>
2016-09-19 14:39:08 -0600

    Specialized Ports

--- java/src/model/Port.java
+++ java/src/model/Port.java
@@ -3,7 +3,8 @@ package model;
 import shared.locations.EdgeLocation;
 
 /**
- * Created by kcwillmore on 9/17/16.
+ * This class represents the game object, ports
+ * Created by jihoon on 9/17/16.
  */
 public class Port {
     private EdgeLocation location;
@@ -11,21 +12,27 @@ public class Port {
      * the resource type that the port can trade for at a 2:1 ratio
      * if the resource type is set to "null" then it is a generic port that instead trades 3:1
      */
-    private ResourceCard.Resource portType;
 
-    public Port(EdgeLocation location, ResourceCard.Resource portType) {
+    /**
+     * Constructor for port
+     * @param location location of the selected port
+     */
+    public Port(EdgeLocation location) {
         this.location = location;
-        this.portType = portType;
     }
 
+    /**
+     * Returns the edge location where the port is located
+     * @return location
+     */
     public EdgeLocation getLocation() {
         return location;
     }
 
-    public ResourceCard.Resource getPortType() {
-        return portType;
-    }
-
+    /**
+     * This function checks if the player can trade with a specific port
+     * @return true if player can trade with this port
+     */
     public boolean canTrade() {
         return false;
     }
