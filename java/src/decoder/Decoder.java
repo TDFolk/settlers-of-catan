9eaced8156378e204130a14d6b2e795042b7d99c
Ji Hoon Park <jihoonpark4829@gmail.com>
2016-09-19 12:40:37 -0600

    DECODER ecksdee

---
+++ java/src/decoder/Decoder.java
@@ -0,0 +1,9 @@
+package decoder;
+
+/**
+ * This class parses the JSON data and also turns objects into JSON objects to send back to the server
+ * Created by jihoon on 9/19/2016.
+ */
+public class Decoder {
+    
+}
