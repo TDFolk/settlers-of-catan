4c92d311ff99e273fcfb0be6c0b067d85fd0b12b
KC Willmore <kcwillmore@KCs-MacBook-Pro.local>
2016-09-19 15:15:36 -0600

    buildings know what color they are

--- java/src/model/Building.java
+++ java/src/model/Building.java
@@ -1,9 +1,10 @@
 package model;
 
+import shared.definitions.CatanColor;
 import shared.locations.VertexLocation;
 
 public abstract class Building {
 	private static Cost cost;
-	
+	private CatanColor color;
 	private VertexLocation location;
 }
