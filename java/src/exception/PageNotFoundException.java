4eb00b5c69db80e14f753573c7f0b1f5a3590ef6
Brandon <bvance800@gmail.com>
2016-09-19 13:48:23 -0600

    Exception
    added pagenotfound exception

---
+++ java/src/exception/PageNotFoundException.java
@@ -0,0 +1,9 @@
+package exception;
+
+/**
+ * Thrown when an error occurs in an HTTP connection that prevents the particular url from being found
+ *
+ * Created by bvanc on 9/19/2016.
+ */
+public class PageNotFoundException extends Exception {
+}
