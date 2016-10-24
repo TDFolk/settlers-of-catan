d4e2a08c497cae2b1366ffe6172bbda1ac7ea710
Ji Hoon Park <jihoonpark4829@gmail.com>
2016-10-24 15:05:50 -0600

    login controller bug fixes... final

--- java/src/client/login/LoginController.java
+++ java/src/client/login/LoginController.java
@@ -118,13 +118,14 @@ public class LoginController extends Controller implements ILoginController, Obs
 			//REGISTER HERE
 			if(Game.getInstance().getServer().userRegister(username, password)){
 
-				System.out.println("IM HERE");
+
 				// If register succeeded
 				getLoginView().closeModal();
 				loginAction.execute();
 			}
 			//if false
 			else {
+
 				messageView.setTitle("Registration Failed");
 				messageView.setMessage("Error in registration.  Try again.");
 				messageView.setController(this);
@@ -154,55 +155,55 @@ public class LoginController extends Controller implements ILoginController, Obs
 
 	private boolean canRegister(String username, String password, String passwordConfirm){
 		if(username.isEmpty() || password.isEmpty()){
-			messageView.setTitle("Registration Error");
-			messageView.setMessage("Enter a valid username and password");
-			messageView.setController(this);
-			messageView.showModal();
+//			messageView.setTitle("Registration Error");
+//			messageView.setMessage("Enter a valid username and password");
+//			messageView.setController(this);
+//			messageView.showModal();
 			return false;
 		}
 
 		//check if username and password are null
 		if(username == null || password == null){
-			messageView.setTitle("Registration Error");
-			messageView.setMessage("Enter a valid username and password");
-			messageView.setController(this);
-			messageView.showModal();
+//			messageView.setTitle("Registration Error");
+//			messageView.setMessage("Enter a valid username and password");
+//			messageView.setController(this);
+//			messageView.showModal();
 			return false;
 		}
 
 		//check if username is 3 to 7 characters
 		if(username.length() < 3 || username.length() > 7){
-			messageView.setTitle("Registration Error");
-			messageView.setMessage("Username must be 3 to 7 characters!");
-			messageView.setController(this);
-			messageView.showModal();
+//			messageView.setTitle("Registration Error");
+//			messageView.setMessage("Username must be 3 to 7 characters!");
+//			messageView.setController(this);
+//			messageView.showModal();
 			return false;
 		}
 
 		//check if password is 5 or more characters
 		if(password.length() < 5){
-			messageView.setTitle("Registration Error");
-			messageView.setMessage("Password must be 5 or more characters!");
-			messageView.setController(this);
-			messageView.showModal();
+//			messageView.setTitle("Registration Error");
+//			messageView.setMessage("Password must be 5 or more characters!");
+//			messageView.setController(this);
+//			messageView.showModal();
 			return false;
 		}
 
 		//check if the passwords are the same
 		if(!password.equals(passwordConfirm)){
-			messageView.setTitle("Registration Error");
-			messageView.setMessage("Please enter the same password.");
-			messageView.setController(this);
-			messageView.showModal();
+//			messageView.setTitle("Registration Error");
+//			messageView.setMessage("Please enter the same password.");
+//			messageView.setController(this);
+//			messageView.showModal();
 			return false;
 		}
 
 		//make sure all fields have only letters, numbers, underscores, dashes
-		if(!username.matches("^[a-zA-Z0-9_-]*$") && !password.matches("^[a-zA-Z0-9_-]*$")){
-			messageView.setTitle("Registration Error");
-			messageView.setMessage("Cannot have invalid symbols!");
-			messageView.setController(this);
-			messageView.showModal();
+		if(!username.matches("^[a-zA-Z0-9_-]*$") || !password.matches("^[a-zA-Z0-9_-]*$")){
+//			messageView.setTitle("Registration Error");
+//			messageView.setMessage("Cannot have invalid symbols!");
+//			messageView.setController(this);
+//			messageView.showModal();
 			return false;
 		}
 		return true;
