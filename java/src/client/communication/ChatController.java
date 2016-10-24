387614cab6626795a13a1d8b8802ee0105e306ce
Nicholas Angell <nrangell94@gmail.com>
2016-10-24 14:34:42 -0600

    Chat and History
    Need more functionality before I can test History fully, but Chat
    should be running?

--- java/src/client/communication/ChatController.java
+++ java/src/client/communication/ChatController.java
@@ -3,18 +3,25 @@ package client.communication;
 import client.base.*;
 import model.Game;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+
+import shared.definitions.CatanColor;
 
 
 /**
  * Chat controller implementation
  */
 public class ChatController extends Controller implements IChatController, Observer {
+	private List<LogEntry> entries;
 
 	public ChatController(IChatView view) {
 		
 		super(view);
+		
+		entries = new ArrayList<LogEntry>();
 		// This Controller will now be notified of any changes in the Game Object
 		Game.getInstance().addObserver(this);
 	}
@@ -26,6 +33,9 @@ public class ChatController extends Controller implements IChatController, Obser
 
 	@Override
 	public void sendMessage(String message) {
+		CatanColor color = Game.getInstance().getPlayer().getColor();
+		entries.add(new LogEntry(color, message));
+		getView().setEntries(entries);
 		
 	}
 
