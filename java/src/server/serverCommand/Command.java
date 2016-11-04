14a8778187f29f7c62a348c1c7141af11ff160b5
Brandon <bvance800@gmail.com>
2016-11-04 16:56:57 -0600

    Command class

--- java/src/server/serverCommand/Command.java
+++ java/src/server/serverCommand/Command.java
@@ -4,4 +4,12 @@ package server.serverCommand;
  * Created by bvanc on 11/4/2016.
  */
 public abstract class Command {
+
+    /**
+     *
+     * @return the entire model formatted as a JsonString
+     */
+    public abstract String execute();
+    // The facade will create the command objects and call their execute methods: joinGameCommand.execute();
+
 }
