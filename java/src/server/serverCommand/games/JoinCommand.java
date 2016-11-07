117bacbe80d387d464d571ac83ccc3006306ec14
KC Willmore <kcwillmore@KCs-MacBook-Pro.local>
2016-11-07 13:46:11 -0700

    Command pattern inheritance

--- java/src/server/serverCommand/games/JoinCommand.java
+++ java/src/server/serverCommand/games/JoinCommand.java
@@ -1,7 +1,26 @@
 package server.serverCommand.games;
 
+import com.google.gson.JsonElement;
+import com.sun.net.httpserver.HttpExchange;
+import server.serverCommand.Command;
+
 /**
  * Created by jihoon on 11/7/2016.
  */
-public class JoinCommand {
+public class JoinCommand extends Command {
+
+    public JoinCommand(HttpExchange httpExchange) {
+        super(httpExchange);
+    }
+
+    /**
+     * This method will handle executing the commands
+     *
+     * @param json jsonString that will populate the JsonElement
+     * @return returns a json element from the given jsonString
+     */
+    @Override
+    public JsonElement execute(String json) {
+        return super.execute(json);
+    }
 }
--- java/src/server/serverCommand/moves/MonopolyCommand.java
+++ java/src/server/serverCommand/moves/MonopolyCommand.java
@@ -1,7 +1,26 @@
 package server.serverCommand.moves;
 
+import com.google.gson.JsonElement;
+import com.sun.net.httpserver.HttpExchange;
+import server.serverCommand.Command;
+
 /**
  * Created by jihoon on 11/7/2016.
  */
-public class MonopolyCommand {
+public class MonopolyCommand extends Command {
+
+    public MonopolyCommand(HttpExchange httpExchange) {
+        super(httpExchange);
+    }
+
+    /**
+     * This method will handle executing the commands
+     *
+     * @param json jsonString that will populate the JsonElement
+     * @return returns a json element from the given jsonString
+     */
+    @Override
+    public JsonElement execute(String json) {
+        return super.execute(json);
+    }
 }
