a337e90286a37bab73d858be19221b92c9ef0403
Ji Hoon Park <jihoonpark4829@gmail.com>
2016-11-07 12:53:07 -0700

    handlers don't need javadoc

---
+++ java/src/server/handler/Handlers.java
@@ -0,0 +1,47 @@
+package server.handler;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+/**
+ * Created by jihoon on 11/7/2016.
+ */
+public abstract class Handlers implements HttpHandler {
+
+    protected String root;
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+    }
+
+    public Handlers(String root){
+        this.root = root;
+    }
+
+    public String getRequestPath(HttpExchange exchange){
+        return exchange.getRequestURI().getPath().substring(1);
+    }
+
+    public void sendFile(HttpExchange exchange, String filePath){
+
+    }
+
+    public void appendJson(){
+
+    }
+
+    public String getLoginCookie(){
+
+        return null;
+    }
+
+    public String getJoinGameCookie(){
+
+        return null;
+    }
+
+
+}
