261c2630e60f93ea1a12239aec5ed51f9b12b1b2
Ji Hoon Park <jihoonpark4829@gmail.com>
2016-11-18 13:38:23 -0700

    list

--- java/src/server/serverCommand/games/ListCommand.java
+++ java/src/server/serverCommand/games/ListCommand.java
@@ -1,26 +1,34 @@
 package server.serverCommand.games;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
 import com.sun.net.httpserver.HttpExchange;
+import command.game.GameListHolder;
+import server.ServerProxy;
 import server.serverCommand.Command;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by jihoon on 11/7/2016.
  */
 public class ListCommand extends Command {
 
-    public ListCommand(HttpExchange httpExchange) {
+    public ListCommand(HttpExchange httpExchange) throws IOException{
         super(httpExchange);
     }
 
     /**
      * This method will handle executing the commands
      *
-     * @param json jsonString that will populate the JsonElement
      * @return returns a json element from the given jsonString
      */
     @Override
     public JsonElement execute() {
-        return null;
+        GameListHolder gameListHolder = ServerProxy.getServer().gameList();
+        return new JsonPrimitive(gson.toJson(gameListHolder));
     }
 }
