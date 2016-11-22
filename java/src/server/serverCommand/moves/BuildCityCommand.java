b4be63edb2a2843220f961d84b6bf3b586a92da5
Ji Hoon Park <jihoonpark4829@gmail.com>
2016-11-21 21:35:47 -0700

    more city

--- java/src/server/serverCommand/moves/BuildCityCommand.java
+++ java/src/server/serverCommand/moves/BuildCityCommand.java
@@ -48,7 +48,7 @@ public class BuildCityCommand extends Command {
         if(super.hasGameCookie && super.hasUserCookie){
 
             JsonMap jsonMap = ServerModel.getInstance().getGame(super.gameId).getMap();
-            ServerModel.getInstance().getGame(super.gameId).getMap().addToArray(jsonMap.getCities(), new JsonPiece(null, 0, buildCityObject.getVertexLocation().getDir().toString(),
+            jsonMap.addToArray(jsonMap.getCities(), new JsonPiece(null, 0, buildCityObject.getVertexLocation().getDir().toString(),
                     new JsonLocation(buildCityObject.getVertexLocation().getHexLoc().getX(), buildCityObject.getVertexLocation().getHexLoc().getY())));
 
             ServerModel.getInstance().getGame(super.gameId).incrementVersion();
