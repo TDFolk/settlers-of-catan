7c070edf96bb3bbc14380461782d66ec4093ddbb
Ji Hoon Park <jihoonpark4829@gmail.com>
2016-11-22 13:09:33 -0700

    gjeroighjo

--- java/src/server/serverCommand/moves/BuildSettlementCommand.java
+++ java/src/server/serverCommand/moves/BuildSettlementCommand.java
@@ -1,6 +1,8 @@
 package server.serverCommand.moves;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
 import com.sun.net.httpserver.HttpExchange;
 
@@ -11,6 +13,7 @@ import server.ServerFacade;
 import server.serverCommand.Command;
 import server.serverModel.ServerGameModel;
 import server.serverModel.ServerModel;
+import shared.locations.VertexDirection;
 import shared.locations.VertexLocation;
 
 /**
@@ -20,13 +23,27 @@ import shared.locations.VertexLocation;
 public class BuildSettlementCommand extends Command {
 
     private BuildSettlementObject buildSettlementObject;
+    private VertexLocation vertexLocation;
+    private VertexDirection vertexDirection;
+    private int x;
+    private int y;
 
     public BuildSettlementCommand(HttpExchange httpExchange) {
         super(httpExchange);
 
         json = json.replaceAll("\\n", "");
+//        String split = json.split("\"");
+
+
         buildSettlementObject = gson.fromJson(json, BuildSettlementObject.class);
 
+
+        JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+        vertexLocation = fetchVertexLocation(jsonObject.get("vertexLocation").getAsJsonObject());
+
+        vertexDirection = vertexLocation.getDir();
+        x = vertexLocation.getHexLoc().getX();
+        y = vertexLocation.getHexLoc().getY();
 
     }
 
@@ -44,10 +61,7 @@ public class BuildSettlementCommand extends Command {
 
             ServerModel.getInstance().getGame(super.gameId).getMap().addToArray(
                     ServerModel.getInstance().getGame(super.gameId).getMap().getSettlements(),
-                    new JsonPiece(null, 0,
-                            super.whatDirection(buildSettlementObject.getVertexLocation().getDir().toString()),
-                            new JsonLocation(buildSettlementObject.getVertexLocation().getHexLoc().getX(),
-                            buildSettlementObject.getVertexLocation().getHexLoc().getY())));
+                    new JsonPiece(null, 0, vertexDirection.toString(), new JsonLocation(x, y)));
 
             ServerModel.getInstance().getGame(super.gameId).incrementVersion();
             String response = ServerModel.getInstance().getGame(super.gameId).getJsonFromModel();
