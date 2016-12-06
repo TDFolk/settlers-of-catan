8d0860b75d4d42646b5014adc3377c5970ff45e9
Ji Hoon Park <jihoonpark4829@gmail.com>
2016-12-05 18:01:47 -0700

    road build, year of plenty

--- java/src/server/serverCommand/moves/RoadBuildingCommand.java
+++ java/src/server/serverCommand/moves/RoadBuildingCommand.java
@@ -1,47 +1,98 @@
 package server.serverCommand.moves;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
 import com.sun.net.httpserver.HttpExchange;
 
 import command.player.RoadBuildingObject;
+import decoder.JsonModels.JsonLocation;
+import decoder.JsonModels.JsonPiece;
 import server.ServerFacade;
 import server.serverCommand.Command;
 import server.serverModel.ServerGameModel;
+import server.serverModel.ServerModel;
+import shared.locations.EdgeDirection;
 import shared.locations.EdgeLocation;
+import shared.locations.HexLocation;
 
 /**
  * Created by jihoon on 11/7/2016.
  */
 public class RoadBuildingCommand extends Command {
 	
-	private int playerIndex;
+//	private int playerIndex;
 	private EdgeLocation spot1;
 	private EdgeLocation spot2;
     private RoadBuildingObject roadBuildingObject;
 
+
     public RoadBuildingCommand(HttpExchange httpExchange) {
         super(httpExchange);
+        json = json.replaceAll("\\n", "");
         roadBuildingObject = gson.fromJson(json, RoadBuildingObject.class);
+
+        //TODO: CHECK THIS SYNTAX
+        JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+
+        JsonObject spot1Object = jsonObject.get("spot1").getAsJsonObject();
+
+        int x = spot1Object.get("x").getAsInt();
+        int y = spot1Object.get("y").getAsInt();
+        String dir = spot1Object.get("direction").getAsString();
+        dir = whatDirection(dir);
+        spot1 = new EdgeLocation(new HexLocation(x, y), EdgeDirection.valueOf(dir));
+
+        JsonObject spot2Object = jsonObject.get("spot2").getAsJsonObject();
+        int x2 = spot2Object.get("x").getAsInt();
+        int y2 = spot2Object.get("y").getAsInt();
+        String dir2 = spot2Object.get("direction").getAsString();
+        dir2 = whatDirection(dir2);
+        spot2 = new EdgeLocation(new HexLocation(x2, y2), EdgeDirection.valueOf(dir2));
+
     }
 
     /**
      * This method will handle executing the commands
      *
-     * @param json jsonString that will populate the JsonElement
      * @return returns a json element from the given jsonString
      */
     @Override
     public JsonElement execute() {
         if(super.hasGameCookie && super.hasUserCookie){
-            String response = ServerFacade.getInstance().roadBuilding(roadBuildingObject.getPlayerIndex(),
-                                                    roadBuildingObject.getSpot1(), roadBuildingObject.getSpot2());
+
+            //decrement road total twice
+            ServerModel.getInstance().getGame(super.gameId).getPlayers()[roadBuildingObject.getPlayerIndex()].decrementRoadTotal();
+            ServerModel.getInstance().getGame(super.gameId).getPlayers()[roadBuildingObject.getPlayerIndex()].decrementRoadTotal();
+
+//            ServerModel.getInstance().getGame(super.gameId).getMap().setRoads()
+
+            ServerModel.getInstance().getGame(super.gameId).getMap().setRoads(ServerModel.getInstance().getGame(super.gameId).getMap().addToArray(
+                    ServerModel.getInstance().getGame(super.gameId).getMap().getRoads(),
+                    new JsonPiece(null, 0, spot1.getDir().toString(), new JsonLocation(spot1.getHexLoc().getX(), spot1.getHexLoc().getY(), spot1.getDir().toString())),
+                    roadBuildingObject.getPlayerIndex()));
+
+
+            ServerModel.getInstance().getGame(super.gameId).getMap().setRoads(ServerModel.getInstance().getGame(super.gameId).getMap().addToArray(
+                    ServerModel.getInstance().getGame(super.gameId).getMap().getRoads(),
+                    new JsonPiece(null, 0, spot1.getDir().toString(), new JsonLocation(spot2.getHexLoc().getX(), spot2.getHexLoc().getY(), spot2.getDir().toString())),
+                    roadBuildingObject.getPlayerIndex()));
+
+
+            String response = ServerModel.getInstance().getGame(super.gameId).getJsonFromModel();
 
             if(response == null){
                 return new JsonPrimitive("Invalid");
             }
             else {
-                // Returns the client model JSON (identical to /game/model)
+                String userName = ServerModel.getInstance().getUsernameFromID(super.playerId);
+                String historyMessage = userName + " has built a road";
+                ServerModel.getInstance().getGame(super.gameId).addLog(historyMessage, userName);
+                ServerModel.getInstance().getGame(super.gameId).addLog(historyMessage, userName);
+
+
+                ServerModel.getInstance().getGame(super.gameId).incrementVersion();
                 return new JsonPrimitive(response);
             }
         }
--- java/src/server/serverCommand/moves/YearOfPlentyCommand.java
+++ java/src/server/serverCommand/moves/YearOfPlentyCommand.java
@@ -1,6 +1,8 @@
 package server.serverCommand.moves;
 
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.google.gson.JsonPrimitive;
 import com.sun.net.httpserver.HttpExchange;
 
@@ -8,6 +10,7 @@ import command.player.YearOfPlentyObject;
 import server.ServerFacade;
 import server.serverCommand.Command;
 import server.serverModel.ServerGameModel;
+import server.serverModel.ServerModel;
 import shared.definitions.ResourceType;
 
 /**
@@ -15,38 +18,75 @@ import shared.definitions.ResourceType;
  */
 public class YearOfPlentyCommand extends Command {
 	
-	private int playerIndex;
-	private ResourceType resource1;
-	private ResourceType resource2;
+//	private int playerIndex;
+//	private ResourceType resource1;
+//	private ResourceType resource2;
+    private String resource1;
+    private String resource2;
     private YearOfPlentyObject yearOfPlentyObject;
 
     public YearOfPlentyCommand(HttpExchange httpExchange) {
         super(httpExchange);
+        json = json.replaceAll("\\n", "");
         yearOfPlentyObject = gson.fromJson(json, YearOfPlentyObject.class);
+
+        JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+        resource1 = jsonObject.get("resource1").getAsString();
+        resource2 = jsonObject.get("resource2").getAsString();
+
     }
 
     /**
      * This method will handle executing the commands
      *
-     * @param json jsonString that will populate the JsonElement
      * @return returns a json element from the given jsonString
      */
     @Override
     public JsonElement execute() {
         if(super.hasGameCookie && super.hasUserCookie){
-            String response = ServerFacade.getInstance().yearOfPlenty(yearOfPlentyObject.getPlayerIndex(),
-                                                    yearOfPlentyObject.getResource1(), yearOfPlentyObject.getResource2());
+
+            incrementCards(resource1);
+            incrementCards(resource2);
+
+            String response = ServerModel.getInstance().getGame(super.gameId).getJsonFromModel();
 
             if(response == null){
                 return new JsonPrimitive("Invalid");
             }
             else {
                 // Returns the client model JSON (identical to /game/model)
+                ServerModel.getInstance().getGame(super.gameId).incrementVersion();
                 return new JsonPrimitive(response);
             }
         }
         else {
             return new JsonPrimitive("catan.game and/or catan.user cookies are missing");
+        }
+    }
+
+    private void incrementCards(String resourceType){
+        int currentAmount;
+        switch(resourceType.toLowerCase()){
+            case "brick":
+                currentAmount = ServerModel.getInstance().getGame(super.gameId).getPlayers()[yearOfPlentyObject.getPlayerIndex()].getResources().getBrick();
+                ServerModel.getInstance().getGame(super.gameId).getPlayers()[yearOfPlentyObject.getPlayerIndex()].getResources().setBrick(currentAmount + 1);
+                break;
+            case "sheep":
+                currentAmount = ServerModel.getInstance().getGame(super.gameId).getPlayers()[yearOfPlentyObject.getPlayerIndex()].getResources().getSheep();
+                ServerModel.getInstance().getGame(super.gameId).getPlayers()[yearOfPlentyObject.getPlayerIndex()].getResources().setSheep(currentAmount + 1);
+                break;
+            case "wood":
+                currentAmount = ServerModel.getInstance().getGame(super.gameId).getPlayers()[yearOfPlentyObject.getPlayerIndex()].getResources().getWood();
+                ServerModel.getInstance().getGame(super.gameId).getPlayers()[yearOfPlentyObject.getPlayerIndex()].getResources().setWood(currentAmount + 1);
+                break;
+            case "wheat":
+                currentAmount = ServerModel.getInstance().getGame(super.gameId).getPlayers()[yearOfPlentyObject.getPlayerIndex()].getResources().getWheat();
+                ServerModel.getInstance().getGame(super.gameId).getPlayers()[yearOfPlentyObject.getPlayerIndex()].getResources().setWheat(currentAmount + 1);
+                break;
+            case "ore":
+                currentAmount = ServerModel.getInstance().getGame(super.gameId).getPlayers()[yearOfPlentyObject.getPlayerIndex()].getResources().getOre();
+                ServerModel.getInstance().getGame(super.gameId).getPlayers()[yearOfPlentyObject.getPlayerIndex()].getResources().setOre(currentAmount + 1);
+                break;
         }
     }
 }
